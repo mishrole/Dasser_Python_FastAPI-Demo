ad0ebac2056422d33690fd99450fccea0594394b
Mitchell Rodríguez <mitrodle@gmail.com>
2022-03-24 20:50:07 -0500

    add: Pydantic BaseModel + Body response

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 # main.py
+
+# Python
+from typing import Optional, List, Dict
+
+# Pydantic
+from pydantic import BaseModel
+
 # Import Module
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from starlette.responses import RedirectResponse
 from starlette.status import HTTP_303_SEE_OTHER
 
 # Instance of FastAPI
 app = FastAPI()
+
+# TODO: Move to /models
+# Models
+
+class User(BaseModel):
+  first_name: str
+  last_name: str
+  age: int
+  title: str
+  # is_married: bool
+  is_married : Optional[bool] = None
+
+users = []
 
 @app.get("/") # Path
 def home():
@@ -30,6 +50,16 @@ def greetings_name_repeat(name: str, repeat: int = 1):
   return {
     "greetings": repeated.rstrip()
   }
+
+@app.get("/users")
+def list_users():
+  return {
+    "users": users
+  }
+
+@app.post("/users")
+def create_user(user: User = Body(...)): # ! with Body(...) model is Required
+  return user
 
 # Run (with hot reloading)
 # uvicorn main:app --reload
